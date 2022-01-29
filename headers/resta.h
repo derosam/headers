55412987b2e189db1017f6020696a7458be51789
derosam <drosas2010@gmail.com>
2022-01-28 20:03:38 -0500

    der

---
+++ headers/resta.h
@@ -0,0 +1,20 @@
+//
+//  resta.h
+//  headers
+//
+//  Created by david on 28/01/22.
+//
+
+//#ifndef resta_h
+//#define resta_h
+
+
+//#endif /* resta_h */
+#include <stdio.h>
+
+int resta(int *uno, int *dos){
+
+int res = *uno - *dos;
+
+    return res;
+}
